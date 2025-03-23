21685a2298794bb58712c0d47ebf48d87689948e
gemtunde <gemtunde@gmail.com>
2025-03-23 16:04:27 +0100

    db setup

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/database");
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Helo from pos server" });
